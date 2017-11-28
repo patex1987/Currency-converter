e4107c158119b020f3ea78542e4c082556340958
patex1987 <patex1987@gmail.com>
2017-11-28 13:42:48 +0100

    generalized convert method is created

--- currency_converter.py
+++ currency_converter.py
@@ -15,12 +15,37 @@ class CurrencyConverter(object):
         Constructor
         '''
         self.actual_rates = self._get_actual_rates()
-        self.symbols_map = self.get_symbols_map()
+        self.symbols_map = self._get_symbols_map()
+        self.available_currencies = self._get_available_currencies()
 
     def convert(self,
                 input_amount,
                 input_currency,
-                output_currency):
+                output_currency=None):
+        '''
+        converts the input amount into a structured output
+        output curency can be a single currency or all available currencies
+        '''
+        output_currencies = self._get_current_outputs(input_currency,
+                                                      output_currency)
+        output_amounts = self._get_all_conversions(input_amount,
+                                                   input_currency,
+                                                   output_currencies)
+        return output_amounts   
+
+    def _get_all_conversions(input_amount, input_currency, output_currencies)
+        output_conversions = {}
+        for currency in output_currencies:
+            output_amount = self.convert_single_currency(input_amount,
+                                                         input_currency,
+                                                         currency)
+            output_conversions[currency] = output_amount
+        return output_conversions
+
+    def convert_single_currency(self,
+                                input_amount,
+                                input_currency,
+                                output_currency):
         '''
         Converts the provided input amount of money into the desired output
         amount
@@ -31,6 +56,27 @@ class CurrencyConverter(object):
         output_amount = self._calculate_output_amount(input_amount,
                                                       actual_conversion_rate)
         return output_amount
+
+    def _get_actual_rates(self):
+        '''
+        Retrieves the actual currency conversion rates from fixer.io
+        '''
+
+    def _get_symbols_map(self):
+        '''
+        gets currency symbols mapping
+        '''
+
+    def _get_available_currencies(self):
+        '''
+        returns a list of available ouput currencies
+        '''
+
+    def _get_current_outputs(self, input_currency, output_currency):
+        '''
+        returns a list of possible output currencies for the selected input
+        currency
+        '''
 
     def _check_rates_actuality(self):
         '''
@@ -49,7 +95,4 @@ class CurrencyConverter(object):
         Returns the output amount
         '''
 
-    def _get_actual_rates(self):
-        '''
-        Retrieves the actual currency conversion rates from fixer.io
-        '''
+
