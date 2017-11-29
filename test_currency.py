7694e790cae5ee8b2bbe9dc268270fc46a004c09
patex1987 <patex1987@gmail.com>
2017-11-29 07:51:20 +0100

    test_limit_reached is deleted
    the limit reached error is handled in the program

--- test_currency.py
+++ test_currency.py
@@ -32,13 +32,3 @@ def test_base_rate(converter):
     assert 'rates' in converter.actual_rates.keys()
     currency_codes = list(converter.actual_rates['rates'].keys())
     assert currency_codes == [converter.base_currency]
-
-
-def test_limit_reached(converter):
-    '''
-    Tests if the CurrencyConverter raises an exception if
-    fixer.io reaches request limit
-    '''
-    with pytest.raises(currency_exceptions.FixerError) as err_inf:
-        for _ in range(100):
-            converter._get_actual_rates()
